4c5ba3422fa49c1042c02c441eebd37f927aeeeb
jumbumbito-sudo <jumbumbito@gmail.com>
2026-02-25 17:34:56 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /**
- * GoopiApp - Core Logic (Tokyo Midnight Pro Edition v6.1)
+ * GoopiApp - Core Logic (Tokyo Midnight Pro Edition v6.2)
  */
 
 const wpConfig = {
@@ -139,17 +139,17 @@ function renderView(view, container) {
                         <i class="fas fa-arrow-left"></i>
                     </button>
                     
-                    <!-- Banners Publicitarios Superiores -->
-                    <div style="height: 150px; width: 100%; background: #000; overflow: hidden; position: relative; border-bottom: 2px solid var(--secondary-lilac);">
+                    <!-- Banners Publicitarios (Corte Preciso -330px para móvil) -->
+                    <div style="height: 130px; width: 100%; background: #fff; overflow: hidden; position: relative; border-bottom: 3px solid var(--secondary-lilac);">
                          <iframe src="https://goopiapp.com/publicidad/" 
-                                style="width: 100%; height: 500px; border: none; position: absolute; top: -110px; left: 0;">
+                                style="width: 100%; height: 600px; border: none; position: absolute; top: -330px; left: 0;">
                          </iframe>
                     </div>
 
-                    <!-- Mapa de Taxis/Delivery -->
-                    <div style="flex: 1; width: 100%; overflow: hidden; position: relative;">
+                    <!-- Mapa (Corte Preciso -150px para móvil) -->
+                    <div style="flex: 1; width: 100%; overflow: hidden; position: relative; background: #000;">
                         <iframe src="https://goopiapp.com/taxis-disponibles/" 
-                                style="width: 100%; height: calc(100% + 110px); border: none; position: absolute; top: -110px; left: 0;" 
+                                style="width: 100%; height: calc(100% + 150px); border: none; position: absolute; top: -150px; left: 0;" 
                                 allow="geolocation">
                         </iframe>
                     </div>
