d4ee76418123c913c43ffd9b4f8241d55a11b1b0
jumbumbito-sudo <jumbumbito@gmail.com>
2026-02-27 15:45:53 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /**
- * GoopiApp - Core Logic (Tokyo Midnight Pro Edition v34.3)
+ * GoopiApp - Core Logic (Tokyo Midnight Pro Edition v34.4)
  */
-console.log("🚀 GOOPIAPP VERSION 34.3 LOADED");
+console.log("🚀 GOOPIAPP VERSION 34.4 LOADED");
 
 const wpConfig = {
     url: "https://goopiapp.com/wp-json",
@@ -566,9 +566,6 @@ function showInfoPopup(page) {
                 <i class="fas fa-times"></i>
             </button>
             ${featuredImg ? `<img src="${featuredImg}" class="popup-img">` : ''}
-            <div style="color: var(--text-dim); line-height: 1.6; font-size: 14px; margin-bottom: 5px;">
-                ${page.content.rendered}
-            </div>
         </div>
     `;
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'goopi-v34.3';
+const CACHE_NAME = 'goopi-v34.4';
 const ASSETS = [
     './',
     './index.html',
