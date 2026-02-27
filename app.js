8a39237e493ca21fa22c0d65dc80128625c78104
jumbumbito-sudo <jumbumbito@gmail.com>
2026-02-27 15:39:35 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /**
- * GoopiApp - Core Logic (Tokyo Midnight Pro Edition v34.2)
+ * GoopiApp - Core Logic (Tokyo Midnight Pro Edition v34.3)
  */
-console.log("🚀 GOOPIAPP VERSION 34.2 LOADED");
+console.log("🚀 GOOPIAPP VERSION 34.3 LOADED");
 
 const wpConfig = {
     url: "https://goopiapp.com/wp-json",
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'goopi-v34.2';
+const CACHE_NAME = 'goopi-v34.3';
 const ASSETS = [
     './',
     './index.html',
